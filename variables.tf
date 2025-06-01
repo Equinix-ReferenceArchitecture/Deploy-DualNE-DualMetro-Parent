b0dd5fd71116a520686b7c9d317bbe306e3ac5d6
vipin2060 <vipinvikraman86@gmail.com>
2025-06-01 09:18:28 +0800

    Create variables.tf
    This is initial uploads of Variables file

---
+++ variables.tf
@@ -0,0 +1,77 @@
+
+
+variable "equinix_client_id" {
+  description = "Equinix client ID (consumer key), obtained after registering app in the developer platform"
+  type        = string
+}
+variable "equinix_client_secret" {
+  description = "Equinix client secret ID (consumer secret), obtained after registering app in the developer platform"
+  type        = string
+}
+
+variable "account_number" {
+  description = "Account Number for the first device" 
+  type        = number
+}
+
+variable "core_count" {
+  description = "Device core"
+  type        = number
+}
+
+variable "metro_code" {
+  description = "Metro in which the first device is deployed" 
+  type        = string
+}
+
+variable "metro_code_secondary" {
+  description = "Metro in which the first device is deployed" 
+  type        = string
+}
+
+variable "type_code" {
+  description = "Device Type" 
+  type        = string
+}
+
+variable "package_code" {
+  description = "Package code" 
+  type        = string
+}
+
+variable "sw_version" {
+  description = "Device Software Version" 
+  type        = string
+}
+
+variable "username" {
+description = "SSH Username" 
+  type        = string
+}
+
+variable "key_name" {
+  description = "Public SSH Key Name" 
+  type        = string
+}
+variable "project_id" {
+description = "Project ID" 
+ type        = string
+}
+
+variable "acl_template_id" {
+  description = "ACL Template for device" 
+  type        = string
+}
+
+variable "notifications" {
+  description = "Email addresses" 
+  type        = list(string)
+}
+
+variable "term_length" {
+  description = "Device core"
+  type        = number
+}
+
+
+
